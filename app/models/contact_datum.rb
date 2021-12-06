ff02071b272e0b9fbe0abc9575ea821f982cfbcf
Your Name <you@example.com>
2021-12-07 01:36:47 +0300

    Fixed a typo in the file names. Extra files removed

--- app/models/contact_datum.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ContactDatum < ApplicationRecord
-  belong_to :user
-end
--- app/models/requirement.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Requirement < ApplicationRecord
-  belong_to :user_setting
-  belong_to :project_condition
-  belong_to :sponsor_condition
-
-  enum type: { project: 0, sponsor: 1 }
-end
